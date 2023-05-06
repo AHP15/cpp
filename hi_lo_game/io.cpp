da4d0da8a8e13d6fbb4c00ce25d358ad8be78db2
Abdessittir <harkati.web.dev@gmail.com>
2023-05-06 17:14:48 +0000

    Add input validation

--- hi_lo_game/io.cpp
+++ hi_lo_game/io.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include<string_view>
+#include <limits>
 
 namespace io {
     void print_message(std::string_view message) {
@@ -9,16 +10,41 @@ namespace io {
     }
 
     int get_user_guess(std::string_view message, int guess_number) {
-        std::cout << message << guess_number << ": ";
-        int input {};
-        std::cin >> input;
-        return input;
+        while(true) {
+            std::cout << message << guess_number << ": ";
+            int input {};
+            std::cin >> input;
+
+            if (std::cin.fail()) {
+                std::cin.clear(); // put us back in 'normal' operation mode
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+            if (input < 1 || input > 100) {
+                std::cin.clear();
+                continue;
+            }
+        
+            return input;
+        }
     }
 
     bool play_again(std::string_view message) {
-        std::cout << message;
-        char input {};
-        std::cin >> input;
-        return input == 'y';
+        while(true) {
+            std::cout << message;
+            char input {};
+            std::cin >> input;
+
+            switch (input) {
+            case 'y':
+               return true;
+            case 'n':
+               return false;
+            default:
+                std::cin.clear(); // put us back in 'normal' operation mode
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+        }
     }
 }
