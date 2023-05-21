90c668a922d48d8f2399937e1cfc7f5fe2419f39
Abdessittir <harkati.web.dev@gmail.com>
2023-05-21 11:37:55 +0100

    Add struct example

---
+++ structs/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct Advertising {
+    int ads_number {};
+    int ad_clicks_number {};
+    double avarage_click_earning {};
+};
+
+void printAdvertising(const Advertising& ads) {
+    std::cout << "The number of ads: " << ads.ads_number << '\n';
+    std::cout << "The number of clicks: " << ads.ad_clicks_number << '\n';
+    std::cout << "The avarge earning per click: " << ads.avarage_click_earning << '\n';
+    std::cout << "The total earning of today: " << ads.ad_clicks_number * ads.avarage_click_earning << '\n';
+}
+
+int main() {
+    Advertising website {};
+    std::cout << "Enter the number of ads: ";
+    std::cin >> website.ads_number;
+
+    std::cout << "Enter the number of clicks: ";
+    std::cin >> website.ad_clicks_number;
+
+    std::cout << "Enter the avarege earning per click: ";
+    std::cin >> website.avarage_click_earning;
+
+    printAdvertising(website);
+    return 0;
+}
