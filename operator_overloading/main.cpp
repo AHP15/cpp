255490dd288fac7e2c1773b843d567ee29adf248
Abdessittir <harkati.web.dev@gmail.com>
2023-06-26 09:57:12 +0100

    Add operator overloading

--- operator_overloading/main.cpp
+++ operator_overloading/main.cpp
@@ -35,6 +35,16 @@ public:
   friend Fraction operator*(const Fraction& f1, const Fraction& f2);
   friend std::ostream& operator<<(std::ostream& out, const Fraction& f);
   friend std::istream& operator>>(std::istream& in, Fraction& f);
+  
+  friend bool operator== (const Fraction& f1, const Fraction& f2);
+	friend bool operator!= (const Fraction& f1, const Fraction& f2);
+
+	friend bool operator< (const Fraction& f1, const Fraction& f2);
+	friend bool operator> (const Fraction& f1, const Fraction& f2);
+
+	friend bool operator<= (const Fraction& f1, const Fraction& f2);
+	friend bool operator>= (const Fraction& f1, const Fraction& f2);
+
 };
 
 Fraction operator*(int integer, const Fraction& f)
@@ -72,17 +82,47 @@ std::istream& operator>>(std::istream& in, Fraction& f)
   return in;
 }
 
+bool operator== (const Fraction& f1, const Fraction& f2)
+{
+	return (f1.m_numerator == f2.m_numerator) && (f1.m_denominator == f2.m_denominator);
+}
+
+bool operator!= (const Fraction& f1, const Fraction& f2)
+{
+	return !(operator==(f1, f2));
+}
+
+bool operator< (const Fraction& f1, const Fraction& f2)
+{
+	return (f1.m_numerator * f2.m_denominator < f2.m_numerator * f1.m_denominator);
+}
+
+bool operator> (const Fraction& f1, const Fraction& f2)
+{
+	return operator<(f2, f1);
+}
+
+bool operator<= (const Fraction& f1, const Fraction& f2)
+{
+	return !(operator>(f1, f2));
+}
+
+bool operator>= (const Fraction& f1, const Fraction& f2)
+{
+	return !(operator<(f1, f2));
+}
+
 int main()
 {
-  Fraction f1;
-	std::cout << "Enter fraction 1: ";
-	std::cin >> f1;
+  Fraction f1{ 3, 2 };
+	Fraction f2{ 5, 8 };
 
-	Fraction f2;
-	std::cout << "Enter fraction 2: ";
-	std::cin >> f2;
-
-	std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n';
+	std::cout << f1 << ((f1 == f2) ? " == " : " not == ") << f2 << '\n';
+	std::cout << f1 << ((f1 != f2) ? " != " : " not != ") << f2 << '\n';
+	std::cout << f1 << ((f1 < f2) ? " < " : " not < ") << f2 << '\n';
+	std::cout << f1 << ((f1 > f2) ? " > " : " not > ") << f2 << '\n';
+	std::cout << f1 << ((f1 <= f2) ? " <= " : " not <= ") << f2 << '\n';
+	std::cout << f1 << ((f1 >= f2) ? " >= " : " not >= ") << f2 << '\n';
 
   return 0;
 }
