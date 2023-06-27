c3f37665f61acee36aa1f345a6b3db461d1dc9d5
Abdessittir <harkati.web.dev@gmail.com>
2023-06-27 15:30:46 +0100

    add fixed floating point

--- array/main.cpp
+++ array/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cassert> // for assert
+#include <cstdint> // for fixed width integers
+#include <cmath> // for std::round()
+
 
 class IntArray
 {
@@ -80,24 +83,106 @@ IntArray fillArray()
 	return a;
 }
 
+class FixedPoint2
+{
+private:
+	std::int16_t m_base{}; // here's our non-fractional part
+	std::int8_t m_decimal{}; // here's our factional part
+
+public:
+	FixedPoint2(std::int16_t base = 0, std::int8_t decimal = 0)
+		: m_base{ base }, m_decimal{ decimal }
+	{
+		// We should handle the case where decimal is > 99 or < -99 here
+		// but will leave as an exercise for the reader
+
+		// If either (or both) of the non-fractional and fractional part of the number are negative, the number should be treated as negative
+		if (m_base < 0 || m_decimal < 0)
+		{
+			// Make sure base is negative
+			if (m_base > 0)
+				m_base = -m_base;
+			// Make sure decimal is negative
+			if (m_decimal > 0)
+				m_decimal = -m_decimal;
+		}
+	}
+
+	FixedPoint2(double d):
+		m_base{ static_cast<std::int16_t>(std::trunc(d)) },
+		m_decimal{ static_cast<std::int8_t>(std::round(d * 100) - m_base * 100) }
+	{
+	}
+
+	operator double() const
+	{
+		return m_base + (static_cast<double>(m_decimal) / 100);
+	}
+
+	friend bool operator==(const FixedPoint2& fp1, const FixedPoint2& fp2)
+	{
+		return (fp1.m_base == fp2.m_base && fp1.m_decimal == fp2.m_decimal);
+	}
+
+	FixedPoint2 operator-() const
+	{
+		// We need to cast, because the negative sign (-) converts our
+		// narrow integers types to int.
+		return {
+			static_cast<std::int16_t>(-m_base),
+			static_cast<std::int8_t>(-m_decimal)
+		};
+	}
+};
+
+// These don't require access to the internals of the class, so they can be defined outside the class
+std::ostream& operator<<(std::ostream& out, const FixedPoint2& fp)
+{
+	out << static_cast<double>(fp);
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, FixedPoint2& fp)
+{
+	double d{};
+	in >> d;
+	fp = FixedPoint2{ d };
+
+	return in;
+}
+
+FixedPoint2 operator+(const FixedPoint2& fp1, const FixedPoint2& fp2)
+{
+	return { static_cast<double>(fp1) + static_cast<double>(fp2) };
+}
+
+void testAddition()
+{
+	// h/t to reader Sharjeel Safdar for this function
+	std::cout << std::boolalpha;
+	std::cout << (FixedPoint2{ 0.75 } + FixedPoint2{ 1.23 } == FixedPoint2{ 1.98 }) << '\n'; // both positive, no decimal overflow
+	std::cout << (FixedPoint2{ 0.75 } + FixedPoint2{ 1.50 } == FixedPoint2{ 2.25 }) << '\n'; // both positive, with decimal overflow
+	std::cout << (FixedPoint2{ -0.75 } + FixedPoint2{ -1.23 } == FixedPoint2{ -1.98 }) << '\n'; // both negative, no decimal overflow
+	std::cout << (FixedPoint2{ -0.75 } + FixedPoint2{ -1.50 } == FixedPoint2{ -2.25 }) << '\n'; // both negative, with decimal overflow
+	std::cout << (FixedPoint2{ 0.75 } + FixedPoint2{ -1.23 } == FixedPoint2{ -0.48 }) << '\n'; // second negative, no decimal overflow
+	std::cout << (FixedPoint2{ 0.75 } + FixedPoint2{ -1.50 } == FixedPoint2{ -0.75 }) << '\n'; // second negative, possible decimal overflow
+	std::cout << (FixedPoint2{ -0.75 } + FixedPoint2{ 1.23 } == FixedPoint2{ 0.48 }) << '\n'; // first negative, no decimal overflow
+	std::cout << (FixedPoint2{ -0.75 } + FixedPoint2{ 1.50 } == FixedPoint2{ 0.75 }) << '\n'; // first negative, possible decimal overflow
+}
+
 int main()
 {
-	IntArray a{ fillArray() };
+	testAddition();
 
-	// If you're getting crazy values here you probably forgot to do a deep copy in your copy constructor
+	FixedPoint2 a{ -0.48 };
 	std::cout << a << '\n';
 
-	auto& ref{ a }; // we're using this reference to avoid compiler self-assignment errors
-	a = ref;
+	std::cout << -a << '\n';
 
-	IntArray b(1);
-	b = a;
+	std::cout << "Enter a number: "; // enter 5.678
+	std::cin >> a;
 
-	a[4] = 7; // change a value in a, b should not change
-
-	// If you're getting crazy values here you probably forgot to do self-assignment check
-	// If b ends in 7, you probably forgot to do a deep copy in your copy assignment
-	std::cout << b << '\n';
+	std::cout << "You entered: " << a << '\n';
 
 	return 0;
 }
